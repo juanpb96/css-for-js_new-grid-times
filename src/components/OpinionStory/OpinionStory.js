8a99acc3de3927ef38c8671e0d8083b1dd34d032
MasterDev <juanpb.96@gmail.com>
2024-07-29 21:05:02 -0500

    Exercise 2: Fix Opinion avatar placement and spacing

--- src/components/OpinionStory/OpinionStory.js
+++ src/components/OpinionStory/OpinionStory.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components/macro';
+import { QUERIES } from '../../constants';
 
 const OpinionStory = ({ id, title, author, avatar }) => {
   return (
@@ -17,6 +18,14 @@ const OpinionStory = ({ id, title, author, avatar }) => {
 
 const Wrapper = styled.article`
   color: var(--color-gray-900);
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: space-between;
+  gap: 32px;
+
+  @media ${QUERIES.laptopAndUp} {
+    gap: 48px;
+  }
 `;
 
 const Avatar = styled.img`
@@ -25,6 +34,7 @@ const Avatar = styled.img`
   height: 48px;
   border-radius: 50%;
   object-fit: cover;
+  margin-top: 3px;
 `;
 
 const AuthorName = styled.p`
