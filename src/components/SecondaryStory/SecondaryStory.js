cc6029ffe8589f11c593af63468123a9aca4069b
MasterDev <juanpb.96@gmail.com>
2024-07-30 19:12:42 -0500

    Exercise 3: Fix swapped styles in SecondaryStory

--- src/components/SecondaryStory/SecondaryStory.js
+++ src/components/SecondaryStory/SecondaryStory.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components/macro';
+import { QUERIES } from '../../constants';
 
 const SecondaryStory = ({ id, title, image, location, abstract }) => {
   return (
@@ -21,6 +22,14 @@ const Wrapper = styled.article`
   gap: 4px 16px;
   grid-template-columns: 120px 1fr;
   color: var(--color-gray-900);
+
+  @media ${QUERIES.tabletOnly} {
+    grid-template-areas: 
+      'image'
+      'heading'
+      'abstract';
+    grid-template-columns: 1fr;
+  }
 `;
 
 const Image = styled.img`
@@ -39,6 +48,10 @@ const Heading = styled.h2`
   line-height: 1.3;
   /* Optical alignment */
   margin-top: -2px;
+
+  @media ${QUERIES.tabletOnly} {
+    margin-top: revert;
+  }
 `;
 
 const Abstract = styled.p`
@@ -48,7 +61,7 @@ const Abstract = styled.p`
   overflow: hidden;
   grid-area: abstract;
   font-size: 1rem;
-  white-space: pre-wrap;
+  white-space: break-spaces;
 `;
 
 export default SecondaryStory;
